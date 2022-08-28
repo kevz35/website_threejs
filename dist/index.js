f217d871ad5e329072529c9e30c2c2afcd1087fc
kevz35 <kevz35@berkeley.edu>
2022-08-28 15:34:15 -0700

    updates

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
-import './style.css';
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
